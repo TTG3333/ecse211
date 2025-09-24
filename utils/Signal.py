1bcd140423c0a6b229872be0291b49927ba065ad
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-09-24 15:01:34 -0400

    Added everything

--- utils/Signal.py
+++ utils/Signal.py
@@ -1,10 +1,10 @@
 import concurrent.futures
 
 from collections.abc import Callable
-from typing import TypeVar, Generic, Dict
+from typing import TypeVar, Generic, Dict, Optional
 
 T = TypeVar('T')
-Callback = Callable[..., None] | None
+Callback = Optional[Callable[..., None]]
 
 pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
 
@@ -20,7 +20,8 @@ class Signal(Generic[T]):
 
     def fire(self, value: T):
         for conn in self.connections.values():
-            pool.submit(conn, value)
+            if conn is not None:
+                pool.submit(conn, value)
 
-    def disconnect(self, value):
+    def disconnect(self, value: int):
         self.connections.pop(value, None)
