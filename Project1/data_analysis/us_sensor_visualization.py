c1985308ccae4e8ec86d81ccc32e36ef19f77ffe
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-09-25 13:11:08 -0400

    Changes to the graph

--- Project1/data_analysis/us_sensor_visualization.py
+++ Project1/data_analysis/us_sensor_visualization.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 
-"""
-This file is used to plot continuous data collected from the ultrasonic sensor.
-It should be run on your computer, not on the robot.
-
-Before running this script for the first time, you must install the dependencies
-as explained in the README.md file.
-"""
-
 from matplotlib import pyplot as plt
 
-US_SENSOR_DATA_FILE = "us_sensor.csv"
 DELAY_SEC = 0.01
+DATA_FILES = ["us_sensor_10cm.csv", "us_sensor_20cm.csv", "us_sensor_30cm.csv"]
 
-with open(US_SENSOR_DATA_FILE, "r") as f:
-    distances = [float(d) for d in f.readlines()]
+distances = []
+for us_sensor_data_file in DATA_FILES:
+    sub = []
+    with open(us_sensor_data_file, "r") as f:
+        sub.extend([float(d) for d in f.readlines()])
+    distances.append(sub)
 
-times = [DELAY_SEC * i for i in range(len(distances))]
+for i, d in enumerate(distances):
+    times = [DELAY_SEC * i for i in range(len(d))]
+    plt.plot(times, d, label=f"Set distance: {(i+1)*10} cm")
 
-# You can customize the plot below, eg, by changing the color
-plt.plot(times, distances)
 plt.xlabel("Time (s)")
 plt.ylabel("Distance (cm)")
-plt.yticks([d for d in range(round(max(distances)) + 10) if d % 10 == 0])
+plt.legend()
+plt.yticks(range(0, 40, 10))
+plt.grid(axis="y")
+plt.tight_layout()
 plt.show()
