5f3fbb9ceed7c63ad4a57a784df90ad6830a8385
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-10-10 10:07:42 -0400

    Updated limits

--- Project1/project/final/drum.py
+++ Project1/project/final/drum.py
@@ -1,11 +1,11 @@
 from utils.brick import Motor
 
 DRUM_MOTOR = Motor("D")
-DRUM_MOTOR.set_limits(power=50, dps=360*6) # (Max 100 BPM)
+DRUM_MOTOR.set_limits(power=100, dps=360*6) # (Max 100 BPM)
 
 def drum_at_bpm(bpm: int):
     # Rotates the drum motor at a certain BPM.
-    DRUM_MOTOR.set_dps(360 * bpm / 60)
+    DRUM_MOTOR.set_dps((-1) * 360 * bpm / 60)
 
 def stop_drum():
     DRUM_MOTOR.set_dps(0)
