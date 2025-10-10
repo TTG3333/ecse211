d11442a1f6d6a19cb32add397af121076f484b63
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-10-10 11:06:50 -0400

    WOW!

--- Project1/project/final/drum.py
+++ Project1/project/final/drum.py
@@ -3,7 +3,7 @@ from utils.brick import Motor
 DRUM_MOTOR = Motor("D")
 DRUM_MOTOR.reset_encoder()
 
-def drum_at_bpm(bpm: int):
+def drum_at_bpm_rot(bpm: int):
     # Rotates the drum motor at a certain BPM.
     DRUM_MOTOR.set_dps(360 * bpm / 60)
 
@@ -14,10 +14,10 @@ if __name__ == "__main__":
     # Test area for drum motor
     import time
 
-    drum_at_bpm(60)
+    drum_at_bpm_rot(60)
     time.sleep(2)
     stop_drum()
     time.sleep(2)
-    drum_at_bpm(200)
+    drum_at_bpm_rot(1000)
     time.sleep(2)
     stop_drum()
