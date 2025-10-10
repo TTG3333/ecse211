32726be5291f003a21e70c4846788114c23a8e46
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-10-10 09:56:17 -0400

    Added some tests

--- Project1/project/final/drum.py
+++ Project1/project/final/drum.py
@@ -9,3 +9,14 @@ def drum_at_bpm(bpm: int):
 
 def stop_drum():
     DRUM_MOTOR.set_dps(0)
+
+
+if __name__ == "__main__":
+    # Test area for drum motor
+    import time
+
+    drum_at_bpm(60)
+    time.sleep(10)
+    stop_drum()
+    time.sleep(2)
+    drum_at_bpm(120)
