6569011855a1f484ceca59943382b2308e5e8855
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-10-10 09:54:57 -0400

    Forgot to add this

--- Project1/project/final/drum.py
+++ Project1/project/final/drum.py
@@ -1,1 +1,11 @@
-git 
+from utils.brick import Motor
+
+DRUM_MOTOR = Motor("D")
+DRUM_MOTOR.set_limits(power=50, dps=360*6) # (Max 100 BPM)
+
+def drum_at_bpm(bpm: int):
+    # Rotates the drum motor at a certain BPM.
+    DRUM_MOTOR.set_dps(360 * bpm / 60)
+
+def stop_drum():
+    DRUM_MOTOR.set_dps(0)
