12b98a3263d0dd6be4483c9ead688445f76f6619
YaroslavMarinenko <37677272+YaroslavMarinenko@users.noreply.github.com>
2025-10-14 18:11:12 -0400

    First (finalized) version of drum.py, needs testing
    45892374503827588354728067895476975406847568975436894758674578367485678476087340567538678954768904576807972385734856758674586785765846748576847645768475687124859678576289567895

--- Project1/project/final/drum.py
+++ Project1/project/final/drum.py
@@ -1,26 +1,60 @@
-from utils.brick import Motor
+from utils.brick import Motor, TouchSensor, wait_ready_sensors
+from utils import sound
+import time
+
+SOUND_1 = sound.Sound(duration=0.1, pitch="A4", volume=85)
+SOUND_2 = sound.Sound(duration=0.1, pitch="C4", volume=85)
+
+EMERGENCY_STOP = TouchSensor(1)
+SPEED_PICKER = TouchSensor(2)
 
 DRUM_MOTOR = Motor("D")
 DRUM_MOTOR.reset_encoder()
 
-def drum_at_bpm_rot(bpm: int):
-    # Rotates the drum motor at a certain BPM.
-    DRUM_MOTOR.set_dps(360 * bpm / 60)
+wait_ready_sensors()
+
+def set_drum_bpm(bpm: int):
+    DRUM_MOTOR.set_dps(6 * bpm)
 
 def stop_drum():
     DRUM_MOTOR.set_dps(0)
 
-# drum_bpm
-# drum_stop
-
 if __name__ == "__main__":
-    # Test area for drum motor
-    import time
+    stop_drum()
+    curr_speed = "off"
 
-    drum_at_bpm_rot(60)
-    time.sleep(2)
-    stop_drum()
-    time.sleep(2)
-    drum_at_bpm_rot(1000)
-    time.sleep(2)
-    stop_drum()
+    while True:
+        if EMERGENCY_STOP.is_pressed():
+            raise Exception("Manual Emergency Exit!")
+
+        elif SPEED_PICKER.is_pressed():
+            while SPEED_PICKER.is_pressed():  # prevents continuous holding of button
+                time.sleep(0.1)
+
+            match curr_speed:
+                case "off":
+                    curr_speed = "slow"
+                    SOUND_1.play()
+                    time.sleep(0.5)
+                    SOUND_2.play()
+                    set_drum_bpm(30)
+                case "slow":
+                    curr_speed = "medium"
+                    SOUND_1.play()
+                    time.sleep(0.3)
+                    SOUND_2.play()
+                    set_drum_bpm(60)
+                case "medium":
+                    curr_speed = "fast"
+                    SOUND_1.play()
+                    time.sleep(0.1)
+                    SOUND_2.play()
+                    set_drum_bpm(120)
+                case "fast":
+                    curr_speed = "off"
+                    SOUND_2.play()
+                    time.sleep(0.5)
+                    SOUND_1.play()
+                    stop_drum()
+                case _:
+                    pass
