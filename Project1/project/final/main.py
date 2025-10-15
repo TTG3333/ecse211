abe6c0a052218c32d836cf36b41e1aa74f9ebd04
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-10-15 14:18:54 -0400

    Slight modification

---
+++ Project1/project/final/main.py
@@ -0,0 +1,14 @@
+import drum
+import note_detection
+import time
+from utils.brick import wait_ready_sensors
+
+def main():
+    wait_ready_sensors()
+    while True:
+        drum.runner()
+        note_detection.runner()
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
