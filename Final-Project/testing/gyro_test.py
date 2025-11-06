88409bb64e50b2d62fa48005851f703580955522
alisamarinenko <alisamarinenko98@gmail.com>
2025-11-06 13:49:24 -0500

    added initial testing

---
+++ Final-Project/testing/gyro_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Testing of the gyro sensor
+"""
+ 
+from utils import sound
+from utils.brick import EV3GyroSensor, wait_ready_sensors
+
+SOUND = sound.Sound(duration=0.3, pitch="A4", volume=85)
+GYRO_SENSOR = EV3GyroSensor(1)
+
+wait_ready_sensors() 
+
+
+def play_sound():
+    "Play a single note."
+    SOUND.play()
+    SOUND.wait_done()
+
+
+if __name__=='__main__':
+    while True:
+        input("lol")
+        print(GYRO_SENSOR.get_abs_measure())
+    
