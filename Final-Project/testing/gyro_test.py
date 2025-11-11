ce744680c83543bf041022a3466e3de392808f05
alisamarinenko <alisamarinenko98@gmail.com>
2025-11-11 12:11:38 -0500

    updated gyro tests

--- Final-Project/testing/gyro_test.py
+++ Final-Project/testing/gyro_test.py
@@ -5,10 +5,12 @@ Testing of the gyro sensor
 """
  
 from utils import sound
-from utils.brick import EV3GyroSensor, wait_ready_sensors
+from utils.brick import Motor, EV3GyroSensor, wait_ready_sensors
 
 SOUND = sound.Sound(duration=0.3, pitch="A4", volume=85)
-GYRO_SENSOR = EV3GyroSensor(3)
+GYRO_SENSOR = EV3GyroSensor(4)
+LEFT_MOTOR = Motor("A")
+RIGHT_MOTOR = Motor("D")
 
 wait_ready_sensors() 
 
@@ -20,7 +22,21 @@ def play_sound():
 
 
 if __name__=='__main__':
+    GYRO_SENSOR.set_mode('abs')
+    offset = 0
     while True:
-        input("lol")
-        print(GYRO_SENSOR.get_abs_measure())
-    
+        i = input("input: ")
+        if i == 'r':
+            offset = GYRO_SENSOR.get_abs_measure()
+            print("measure reset")
+        elif i == '1' or i == '-1':
+            offset = GYRO_SENSOR.get_abs_measure()
+            LEFT_MOTOR.set_dps(200 * int(i))
+            RIGHT_MOTOR.set_dps(-200 * int(i))
+            while abs((GYRO_SENSOR.get_abs_measure() - offset)) < 90:
+                print("current degrees turned: " + str(GYRO_SENSOR.get_abs_measure() - offset))
+            LEFT_MOTOR.set_dps(0)
+            RIGHT_MOTOR.set_dps(0)
+        else:
+            print("adjusted gyro sensor value read: " + str(GYRO_SENSOR.get_abs_measure() - offset))
+        
