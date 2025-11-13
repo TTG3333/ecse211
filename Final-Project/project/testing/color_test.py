92e6b5b63a6b423773b4ce2de1f7b3b41fa7d361
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-11-13 16:05:56 -0500

    Changes

--- Final-Project/project/testing/color_test.py
+++ Final-Project/project/testing/color_test.py
@@ -14,6 +14,14 @@ C_SENSOR = EV3ColorSensor(1)
 wait_ready_sensors()
 
 if __name__=='__main__':
+
+    def mean(vals):
+        return sum(vals) / len(vals)
+    
+    def var(vals):
+        m = mean(vals)
+        return sum((x - m) ** 2 for x in values) / len(values)
+    
     list = []
     for i in range(0, 60):
         r,g,b = C_SENSOR.get_rgb()
@@ -23,12 +31,12 @@ if __name__=='__main__':
 
     # Find the highest and lowest value
     values = [c.value for c in list]
-    print(f"(VALUES) Max: {max(values)}, Min: {min(values)}, Avg: {np.average(values)}, Var: {np.var(values)}")
+    print(f"(VALUES) Max: {max(values)}, Min: {min(values)}, Avg: {mean(values)}, Var: {var(values)}")
 
     # Find the highest saturation
     saturations = [c.saturation for c in list]
-    print(f"(SATURATION) Max: {max(saturations)}, Min: {min(saturations)}, Avg: {np.average(saturations)}, Var: {np.var(saturations)}")
+    print(f"(SATURATION) Max: {max(saturations)}, Min: {min(saturations)}, Avg: {mean(saturations)}, Var: {var(saturations)}")
 
     # Find the highest hue
     hues = [c.hue for c in list]
-    print(f"(HUES) Max: {max(hues)}, Min: {min(hues)}, Avg: {np.average(hues)}, Var: {np.var(hues)}")
+    print(f"(HUES) Max: {max(hues)}, Min: {min(hues)}, Avg: {mean(hues)}, Var: {var(hues)}")
