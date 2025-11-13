d9c3d92dc08d77b89c32f71e7db1fc8e2d8a3b5b
alisamarinenko <alisamarinenko98@gmail.com>
2025-11-13 14:18:01 -0500

    first test of line follower + turn

--- Final-Project/project/testing/__init__.py
+++ Final-Project/project/testing/__init__.py
@@ -1,4 +1,7 @@
 from utils.brick import wait_ready_sensors, TouchSensor, EV3UltrasonicSensor
-from . import line_follower
+from . import line_follower, gyro_turn
 
 line_follower.run()
+gyro_turn.turn_90_deg('left')
+line_follower.run()
+
---
+++ Final-Project/project/testing/gyro_turn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Testing of the gyro sensor
+"""
+ 
+from utils import sound
+from utils.brick import Motor, EV3GyroSensor, wait_ready_sensors
+
+SOUND = sound.Sound(duration=0.3, pitch="A4", volume=85)
+GYRO_SENSOR = EV3GyroSensor(4)
+LEFT_MOTOR = Motor("A")
+RIGHT_MOTOR = Motor("D")
+
+wait_ready_sensors() 
+
+def turn_90_deg(direction: str):
+    i = 1 if direction.lower() == "left" else -1 if direction.lower() == "right" else 0
+    offset = GYRO_SENSOR.get_abs_measure()
+    LEFT_MOTOR.set_dps(200 * int(i))
+    RIGHT_MOTOR.set_dps(-200 * int(i))
+    while abs((GYRO_SENSOR.get_abs_measure() - offset)) < 90 + (3 * i):
+        print("current degrees turned: " + str(GYRO_SENSOR.get_abs_measure() - offset))
+    LEFT_MOTOR.set_dps(0)
+    RIGHT_MOTOR.set_dps(0)
+
