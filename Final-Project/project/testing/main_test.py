ac64f702219985ffe17f6a168f78e9451c32a217
Alexander Stoyanov <stoyanov070@gmail.com>
2025-11-19 14:14:18 -0500

    added restricted room check in main_test.py

--- Final-Project/project/testing/main_test.py
+++ Final-Project/project/testing/main_test.py
@@ -10,6 +10,7 @@ SOUND = sound.Sound(duration=0.3, pitch="A4", volume=85)
 GYRO_SENSOR = EV3GyroSensor(4)
 LEFT_MOTOR = Motor("A")
 RIGHT_MOTOR = Motor("D")
+C_SENSOR = EV3ColorSensor(1)
 
 DISTANCE_TO_WALL = 12
 distances_to_turn_at = [37.6, 37.6, 37.6, 86.1]  # Stack for all 4 rooms
@@ -17,9 +18,30 @@ distances_to_turn_at = [37.6, 37.6, 37.6, 86.1]  # Stack for all 4 rooms
 #naive path function, goes to all rooms, should be made smarter later
 successfulDeliveries = 0 
 
-def check_for_restricted_room():
-    '''Checks for red square'''
-    print("")
+def restricted_room():
+    restricted = false
+    #  move slightly forward (check for red square)
+    LEFT_MOTOR.set_dps(-BACKUP_SPEED)
+    RIGHT_MOTOR.set_dps(-BACKUP_SPEED)
+    time.sleep(0.5)
+    line_follower.stop_robot()
+    #  checks for red square
+    r, g, b, lum = C_SENSOR.get_value()
+    color = Color(r, g, b)
+    if str(color).lower() == "red":
+        restricted = true
+    return restricted
+
+def enter_next_room():
+    line_follower.run(distances_to_turn_at.pop() - 5)
+    gyro_turn.turn_90_deg('left')
+    line_follower.run('orange')
+    line_follower.stop_robot()  # stop at the orange door, then enter intra-room subsystem
+
+def exit_current_room():
+    gyro_turn.turn_180_deg()
+    line_follower.run(DISTANCE_TO_WALL - 4)
+    gyro_turn.turn_90_deg('left')
 
 def deliver_to_next_room():
     '''Enters and exits the next room'''
