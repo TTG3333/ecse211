c12a8c9aaffd7f63c092c254cb87cb357aee909a
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-11-19 17:39:24 -0500

    Added deriv function

--- Final-Project/project/testing/utils/dnoise.py
+++ Final-Project/project/testing/utils/dnoise.py
@@ -2,12 +2,16 @@ import time
 
 class dNoise:
     def __init__(self, max_slope, max):
-        self.time = time.monotonic()
+        self.time = 0
         self.max = max
         self.max_slope = max_slope
         self.values = []
 
     def add(self, val):
+        '''
+        Adds a value to the buffer only if the derivative between that 
+            value and the last one is acceptable
+        '''
         current = time.monotonic()
         dt = current - self.time
         self.time = current
@@ -17,11 +21,19 @@ class dNoise:
             return
         
         latest = self.values[len(self.values) - 1]
-        if abs(val - latest) / dt < self.max_slope:
-            self.values.append(val)
+        if abs(val - latest) / dt > self.max_slope:
+            return False
         
-        if len(self.values) > (self.max - 1):
+        self.values.append(val)
+        if len(self.values) > self.max:
             self.values.pop(0)
+
+        return True
+    
+    def derivative(self, value):
+        dt = time.monotonic() - self.time
+        latest = self.values[len(self.values) - 1]
+        return abs(value - latest) / dt
 
     def median(self):
         vals = sorted(self.values)
