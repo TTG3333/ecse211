055627f004dde2edeedb599a1a1204613ed09a9d
matthew-engi <matthew.lacasse2@mail.mcgill.ca>
2025-11-26 17:43:18 -0500

    Added more documentation and added daemon

--- Final-Project/project/testing/main_test2.py
+++ Final-Project/project/testing/main_test2.py
@@ -4,14 +4,15 @@
 
 from utils.brick    import Motor, EV3GyroSensor, EV3UltrasonicSensor, EV3ColorSensor, TouchSensor, wait_ready_sensors
 
+from utils.sounds   import play_clear, play_estop
+from utils.room     import init_r, handle_room, stop_room
 from utils.driver   import init_d, follow_line, stop_moving
 from utils.turning  import init_t, turn_until_combined, stop_turning
-from utils.room     import init_r, handle_room, stop_room
-from utils.sounds   import play_clear, play_estop
 
-import simpleaudio  as sa
 from time           import sleep
-from threading      import Thread
+
+import threading
+import simpleaudio  as sa
 
 # ---------------------------------------------------- #
 # Configurable settings
@@ -27,9 +28,6 @@ LEFT_MOTOR          = Motor("A")
 PACKAGE_MOTOR       = Motor("B")
 RIGHT_MOTOR         = Motor("D")
 
-# Initialization Configuration
-INITIALIZER         = (COLOR_SENSOR, GYRO_SENSOR, US_SENSOR, LEFT_MOTOR, RIGHT_MOTOR, PACKAGE_MOTOR)
-
 # E-Stop Configuration
 ESTOP_POLL_SLEEP = 0.1
 
@@ -37,54 +35,74 @@ ESTOP_POLL_SLEEP = 0.1
 ROOMS               = [[82, 33], [33], [33], []]
 WALL_DISTANCE       = 8
 END_ROOM_DISTANCE   = 57.5
+
+# Initialization Configuration
+INITIALIZER         = (COLOR_SENSOR, GYRO_SENSOR, US_SENSOR, LEFT_MOTOR, RIGHT_MOTOR, PACKAGE_MOTOR)
 # ---------------------------------------------------- #
 
-FINISHED = False
-
 def estop_handler():
-    global FINISHED
     while True:
-        if FINISHED:
-            break
         if TOUCH_SENSOR.is_pressed():
-            sa.stop_all()
-            s = play_estop()
-            stop_moving()
-            stop_turning()
-            stop_room()
-            s.wait_done()
-            raise Exception
+            break
         sleep(ESTOP_POLL_SLEEP)
 
+    raise Exception
+
 def room_procedure():
+    # Navigate to the orange
     turn_until_combined(direction='left',   colors_list=[["Black"], ["White"]])
     follow_line(until_colors=["Orange"])
+    
+    # Scan the room and deliver package if applicable
     delivered = handle_room()
+
+    # Turn back to exit the room
     turn_until_combined(direction='right',  colors_list=[["Black"], ["White"]])
     follow_line(until_distance=WALL_DISTANCE)
+    
+    # Get back on the original path
     turn_until_combined(direction='left',   colors_list=[["Black"], ["White"]])
     return delivered
 
+def handle_crash(args):
+    # Stop all movement
+    stop_moving(); stop_turning(); stop_room()
+
+    # Play Emergency Sound
+    sa.stop_all()
+    play_estop().wait_done()
+
+    raise args.exc_value # Propagate error to close program
+
 if __name__ == "__main__":
+    # Wait for all initialization of sensors.
     wait_ready_sensors()
-    t = Thread(target=estop_handler)
+
+    # Starts listening to the Emergency Stop
+    threading.excepthook = handle_crash
+    t = threading.Thread(target=estop_handler, daemon=True)
     t.start()
+
+    # Initializes all subsystems
     init_d(*INITIALIZER)
     init_t(*INITIALIZER)
     init_r(*INITIALIZER)
 
+    # Navigate through all the rooms 
     for line in ROOMS:
         for room_dist in line:
             # Follow the line and enter the room
             follow_line(until_distance=room_dist)
             print(f"Package delivered: {room_procedure()}")
 
-        # No more rooms, navigate to the end of the line and turn left.
+        # No more rooms in the line, navigate to the end of the line and turn left.
         follow_line()
         turn_until_combined(direction='left', colors_list=[["Black"], ["White"]])
+
+    # Navigate to the blue area
     follow_line(until_distance=END_ROOM_DISTANCE)
     turn_until_combined(direction='left', colors_list=[["Black"], ["White"]])
     follow_line(until_colors=["Blue"], delay=3)
-    FINISHED = True
+
+    # Play the clear sound and exit the program
     play_clear().wait_done()
-    t.join()
