6ceefff4017e0814907d739b643ab4f02dfd0303
TTG3333 <65309965+TTG3333@users.noreply.github.com>
2025-12-01 22:57:37 -0500

    Better turning

--- Final-Project/project/code/utils/room.py
+++ Final-Project/project/code/utils/room.py
@@ -12,15 +12,16 @@ from utils.sounds   import play_collect
 from utils.driver   import drive_distance, drive_straight, move_precise_distance
 from utils.turning  import turn_angle
 from time           import sleep
-from math           import pi, sqrt, tan, atan
+from math           import pi
 
 # ---------------------------------------------------- #
 # Configurable settings
 
 # Turning Configurations
-START_ANGLE = -40
-END_ANGLE   = 40
-ANGLE_STEP  = 5
+TURN_ANGLES = [-45, -37] + list(range(-30, 31, 6)) + [37, 45]
+START_ANGLE = TURN_ANGLES[0]
+END_ANGLE   = TURN_ANGLES[-1]
+ANGLE_OFFSETS = [TURN_ANGLES[i+1] - TURN_ANGLES[i] for i in range(len(TURN_ANGLES)-1)] + [0] # Don't turn after the last ray
 
 # Certainty Behavior
 COLOR_CERTAINTY = True
@@ -80,15 +81,11 @@ def handle_room():
 
     # Start at -30, end at 30, sensor is clockwise
     delivered = False
-    current = 0
 
     turn_angle(abs(START_ANGLE), direction='left')
     start = GYRO_SENSOR.get_abs_measure()
 
-    for _ in range(START_ANGLE, END_ANGLE, ANGLE_STEP):
-        current += ANGLE_STEP
-
-        # The square is at least 2 inches away from the wall
+    for offset in ANGLE_OFFSETS: # The square is at least 2 inches away from the wall
         _, color = drive_straight(None, until_colors=["White", "Green", "Black", "Blue"])
 
         # If green square detected
@@ -112,7 +109,7 @@ def handle_room():
         if abs(start - GYRO_SENSOR.get_abs_measure()) > abs(START_ANGLE - END_ANGLE):
             break
         
-        turn_angle(abs(ANGLE_STEP), direction='right')
+        turn_angle(abs(offset), direction='right')
     
     offset = GYRO_SENSOR.get_abs_measure() - zero
 
